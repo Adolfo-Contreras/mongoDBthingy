7fb102f6badf414fff0a2aeb31496afedf55ee57
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2024-02-15 10:40:16 -0700

    idk

--- config/database.js
+++ config/database.js
@@ -1,2 +1,2 @@
-const mongoDB_URL='mongodb://localhost:27017/mtech'
+const mongoDB_URL='mongodb+srv://AdolfoAAC:XmaDObDax48dDVc3@cluster1.mru3rgd.mongodb.net/'
 module.exports ={mongoURI:mongoDB_URL}
